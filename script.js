68c9688ed6cfb375bcdef8f0eb4480a405a34350
sanjeevikumar962004-collab <sanjeevikumar962004@gmail.com>
2026-07-23 12:17:54 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -77,9 +77,9 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
     .fromTo('.sky',     { y: 0 },   { y: -200 }, 0)
-    .fromTo('.cloud1',  { y: 100 }, { y: -800 }, 0)
-    .fromTo('.cloud2',  { y: -150 },{ y: -500 }, 0)
-    .fromTo('.cloud3',  { y: -50 }, { y: -650 }, 0)
+    .fromTo('.cloud1',  { y: -50 }, { y: -950 }, 0)
+    .fromTo('.cloud2',  { y: -300 },{ y: -650 }, 0)
+    .fromTo('.cloud3',  { y: -200 },{ y: -800 }, 0)
     .fromTo('.mountBg', { y: -10 }, { y: -100 }, 0)
     .fromTo('.mountMg', { y: -30 }, { y: -250 }, 0)
     .fromTo('.mountFg', { y: -50 }, { y: -600 }, 0);
